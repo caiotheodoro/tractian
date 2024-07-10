474dafbd6980bb67b611ba84d4e60113d304d694
Caio Theodoro <dev.caiotheodoro@gmail.com>
2024-07-10 19:02:34 -0300

    feat: 🎸 layout + pages related

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
-
+import styles from '@/app/page.module.css';
 export default function Home() {
   return (
-    <></>
+    <div className={styles.container}>
+        <h1>No Company Selected!</h1>
+        <p>Please select a company to view its assets and locations</p>
+    </div>
   );
 }
