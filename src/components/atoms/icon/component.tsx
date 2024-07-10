7c0a1db50dc34ed7ebbf44b3b99a39352f9a2dad
Caio Theodoro <dev.caiotheodoro@gmail.com>
2024-07-10 19:00:16 -0300

    feat: 🎸 icon atoms + assets

--- src/components/atoms/icon/component.tsx
+++ src/components/atoms/icon/component.tsx
@@ -1,14 +1,14 @@
 import Image from 'next/image'
 import React from 'react'
 
-export default function LocationIcon({
+export default function ComponentIcon({
   width = 20,
   height = 20,
   className
 }:Readonly<IconProps>) {
   return (
     <Image
-      src="/location.png"
+      src="/assets/component.png"
       alt="Location Icon"
       width={width}
       height={height}
